8a314c20c54a58514f9e9f8e6b9726729bf4d1b2
BuickSalesman <adam.odonnell.e@gmail.com>
2024-06-22 17:12:12 -0400

    deliberate practice

---
+++ 062224.py
@@ -0,0 +1,51 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+
+number = 2
+print(number)
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = "hello"
+print(word.upper())
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+number1 = 5
+number2 = 10
+print(number1 + number2)
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+word = "proton"
+print(word[::-1])
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+number = 19
+print(number * 10)
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "proton"
+word2 = "neutron"
+print(word1.upper() + word2.upper())
+
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word = "proton"
+print(len(word))
+
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+number = 9.99
+print(int(number))
+
+# Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+number1 = 22
+number2 = 20
+print(number2 * number1)
+
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+word = "PROTON"
+print(word.lower())
