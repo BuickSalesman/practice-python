dfcb28db0e681d435531768d17ab0d6ce92d255a
BuickSalesman <adam.odonnell.e@gmail.com>
2024-08-01 15:00:20 -0400

    deliberate practice

---
+++ 080124.py
@@ -0,0 +1,84 @@
+# Write a Song class with attributes for name, artist, and duration.
+class Song:
+    def __init__(self, name, artist, duration):
+        self.name = name
+        self.artist = artist
+        self.duration = duration
+
+# Write a Rectangle class with attributes for width and height.
+
+
+class Rectanlge:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+# Write a Person class with attributes for name and age.
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Write a Location class with attributes for latitude and longitude.
+
+
+class Location:
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
+# Write an Account class with attributes for name and balance.
+
+
+class Account:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+# Write a Movie class with attributes for title, director, and year.
+
+
+class Movie:
+    def __init__(self, title, director, year):
+        self.title = title
+        self.director = director
+        self.year = year
+
+# Write a Car class with attributes for make, model, year, and color.
+
+
+class Car:
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+# Write a Point class with attributes for x, y, and z coordinates.
+
+
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+# Write a Book class with attributes for title, author, and year.
+
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+# Write a Plant class with attributes for name, size, and price.
+
+
+class Plant:
+    def __init__(self, name, size, price):
+        self.name = name
+        self.size = size
+        self.price = price
