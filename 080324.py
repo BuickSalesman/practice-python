5d91c01deb7305ab599319d7305896806d5b4bd0
BuickSalesman <adam.odonnell.e@gmail.com>
2024-08-03 09:37:44 -0400

    deliberate practice

---
+++ 080324.py
@@ -0,0 +1,52 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+
+number = 2
+print(number)
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = "hello"
+print(word.upper())
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+number1 = 5
+number2 = 11
+print(number1 + number2)
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+word = "hello"
+print(word[::-1])
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+number = 100
+print(number * 10)
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "hello"
+word2 = "world"
+print(word1.upper() + word2.upper())
+
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word = "hello"
+print(len(word))
+
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+number = 19.99
+print(int(number))
+
+# Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+number1 = 10
+number2 = 20
+print(number1 * number2)
+
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+word = "HeLlO"
+print(word.lower())
