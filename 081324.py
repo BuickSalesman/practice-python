0e487a5a82d3513bbe323d0e62f648abf5ee426e
BuickSalesman <adam.odonnell.e@gmail.com>
2024-08-13 20:50:58 -0400

    deliberate practice

---
+++ 081324.py
@@ -0,0 +1,61 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+
+number = 1
+print(number + 10)
+
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = "shipping"
+print(word.upper())
+
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+number1 = 3
+number2 = 5
+print(number1 + number2)
+
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+word = "purse"
+print(word[::-1])
+
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+number = 44
+print(number * 10)
+
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "yellow"
+word2 = "flower"
+print(word1.upper() + word2.upper())
+
+
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word = "outstanding"
+print(len(word))
+
+
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+number = 5.69
+print(int(number))
+
+
+# Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+number1 = 33
+number2 = 111
+print(number1 * number2)
+
+
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+word = "paGoLiCCi"
+print(word.lower())
