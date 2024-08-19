4ffbde21426ba46d013e66e27cacb9f0bf467b20
BuickSalesman <adam.odonnell.e@gmail.com>
2024-08-19 17:56:43 -0400

    deliberate practice finishe later

---
+++ 081924.py
@@ -0,0 +1,35 @@
+# Write a while loop to print the numbers 1 through 10.
+
+index = 1
+while index <= 10:
+    print(index)
+    index += 1
+
+# Write a while loop that prints the word "hello" 5 times.
+
+index = 0
+while index < 5:
+    print("hello")
+    index += 1
+
+
+# Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+while True:
+    word = input("Please enter a word: ")
+    if word == "stop":
+        break
+
+# Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+# Write a while loop that prints the number 9000 ten times.
+
+# Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# Write a while loop that prints the numbers 50 to 70.
+
+# Write a while loop that prints the phrase "Around the world" 144 times.
+
+# Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# Write a while loop that prints the even numbers from 2 to 40.
