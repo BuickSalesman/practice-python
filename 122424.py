47a47b47f0ca20adff69eafe0a3627d190c8469a
BuickSalesman <adam.odonnell.e@gmail.com>
2024-12-24 15:52:29 -0500

    deliberate practice

---
+++ 122424.py
@@ -0,0 +1,88 @@
+# Write a method that takes in a number and returns the number times two. Then run the method and print the result.
+
+def times_two(number):
+    return number * 2
+
+
+print(times_two(3))
+
+# Write a method that takes in a string and returns the string with all capital letters. Then run the method and print the result.
+
+
+def capitalize(string):
+    return string.upper()
+
+
+print(capitalize("hello"))
+
+# Write a method that takes in two numbers and returns the first number subtracted by the second. Then run the method and print the result.
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+print(subtract(3, 2))
+
+# Write a method that takes in a number and returns the number times itself. Then run the method and print the result.
+
+
+def square(number):
+    return number * number
+
+
+print(square(4))
+
+# Write a method that takes in a string and returns the first letter of the string. Then run the method and print the result.
+
+
+def first_letter(string):
+    return string[0]
+
+
+print(first_letter("hello"))
+
+# Write a method that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the method and print the result.
+
+
+def string_combine(str1, str2, str3):
+    return str1 + " " + str2 + " " + str3
+
+
+print(string_combine("ur", "mom", "gay"))
+
+# Write a method that takes in a number and returns the number as a string. Then run the method and print the result.
+
+
+def stringify(number):
+    return str(number)
+
+
+print(stringify(4))
+
+# Write a method that takes in a string and returns the string repeated 5 times. Then run the method and print the result.
+
+
+def repeater(string):
+    return string * 5
+
+
+print(repeater("hello"))
+
+# Write a method that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the method and print the result.
+
+
+def average(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+
+
+print(average(3, 5, 8))
+
+# Write a method that takes in a number and returns the number times 10 plus 30. Then run the method and print the result.
+
+
+def math(number):
+    return number * 10 + 30
+
+
+print(math(3))
